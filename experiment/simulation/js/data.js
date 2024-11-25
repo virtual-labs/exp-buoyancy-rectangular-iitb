3522c555556f2cd01a2f1ca39b7c0a5decf03bec
pushpdeep <>
2024-11-25 06:43:59 +0000

    added payload

---
+++ experiment/simulation/js/data.js
@@ -0,0 +1,10 @@
+let block_rho;
+let block_height = parseFloat((Math.random() * 0.8 + 1.2).toFixed(1));
+let block_width = parseFloat((Math.random() * 0.8 + 2.2).toFixed(1));
+let block_length = parseFloat((Math.random() * 0.8 + 5.8).toFixed(1));
+let gravity = 9.81;
+let block_weight = 0;
+let block_submerged_height = 0;
+let water_rho = 1000;
+let cb = 0;
+//# sourceMappingURL=data.js.map
